c6cfda6d5419c5af038777e2f25f379c98049606
Catherine <ducharme.catherine@gmail.com>
2015-09-18 19:50:40 +0000

    Created basic object constructor, fixed promisify module variable

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ var request = require('request');
 var inquirer = require('inquirer');
 var cliTable = require('cli-table');
 
-//prompt = Promise.promisifyAll('prompt');
-//request = Promise.promisify('request');
+prompt = Promise.promisifyAll(prompt);
+request = Promise.promisify(request);
+
+//Address book constructor:
+
+function addressBook (fn, ln, /*bd,*/ a1, c ) {
+        this.firstName = fn;
+        this.lastName = ln;
+        //this.birthday = bd;
+        this.address1 = a1;
+        this.city = c;
+}
+
 
 //Main menu
 
@@ -14,30 +25,44 @@ function mainMenu() {
     console.log('To create a new address book entry, press C.')
     console.log('To search for existing address book entries, press S.')
     console.log('To exit the program, press X.')
+    
+//Prompting the user for what they want to do:
+
+    inquirer.prompt(['Your choice'], function( err, res ) {
+        switch(res) {
+            case ('C'):
+                creator();
+                break;
+            case ('S'):
+                searcher();
+                break;
+            case ('X'):
+                quitter();
+                break;
+            default:
+                console.log('Learn to spell.')
+                break;
+        }
+    }).catch(function(err){
+        console.log("There's something wrong!\n" + err);
+    })
 }
 
-inquirer.prompt.getAsync(['Choice'], function( answers ) {
-    switch(answers) {
-        case ('C'):
-            creator();
-            break;
-        case ('S'):
-            searcher();
-            break;
-        case ('X'):
-            quitter();
-            break;
-        default:
-            console.log('Learn to spell.')
-            break;
-    } 
-})
+//The creator function is used to create new entries in the address book:
 
 function creator() {};
 
-function searcher() {};
+//The searcher function is used to search entries in the address book:
 
-function quitter() {};
+function searcher() {
+    prompt.get(['enter search item here: '], function(err1, res1){
+        var userInput = res1;
+    }).then //search in addressBook object
+};
+
+//The quitter function is used to leave the address book:
+
+function quitter(){};
 
 mainMenu()
 
